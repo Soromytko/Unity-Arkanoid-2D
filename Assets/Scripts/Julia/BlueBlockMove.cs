ce715cef26c41b8bcfb56d5e38e82e47ab5496fb
Soromytko <NikitaSoromytko@gmail.com>
2022-11-26 04:24:57 +0300

    fix: BlueBlock

--- Assets/Scripts/Julia/BlueBlockMove.cs
+++ Assets/Scripts/Julia/BlueBlockMove.cs
@@ -9,20 +9,11 @@ public class BlueBlockMove : MonoBehaviour
     
     void Start()
     {
-        point = transform.position.x;
-        if (point > -1.2f & point < 0)
-            transform.position = new Vector3(-1.5f, 0, 0);
-        else if (point < 1.2f & point > 0)
-            transform.position = new Vector3(1.5f, 0, 0);
-        else if (point <-4f)
-            transform.position = new Vector3(Random.Range(-4,-2), 0, 0);
-        else if (point > 4f)
-            transform.position = new Vector3(Random.Range(2,4), 0, 0);
-        point = transform.position.x;
-
+        //«адаетс€ начальна€ скорость. —лучайно выбираетс€ число от 1 до 6
         speedV = Random.Range(1, 6);
 
         System.Random rand = new System.Random();
+        //¬ыбираетс€ направление движени€. ѕри скорости с отрицательным значением движетс€ влево, с положительным вправо.
         int[] a = new int[2] { -1, 1 };
         speedV = speedV * a[rand.Next(0, a.Length)];
     }
@@ -30,30 +21,15 @@ public class BlueBlockMove : MonoBehaviour
     
     void Update()
     {
-        
+        //Ѕлок движетс€ по оси х
         transform.Translate(speedV * Time.deltaTime, 0, 0);
-        if (speedV > 3 || speedV < -3)
+        //ѕри достижении одного значений -5.8f или 5.8f блок мен€ет направление движени€ 
+        if (transform.position.x > 5.8f || transform.position.x < -5.8f )
         {
-            if (transform.position.x > 5.5f || transform.position.x < -5.5f)
-            {
-                speedV = -speedV;
-            }
-        }
-        else
-        {
-            if (point < 0 & point > -5.5)
-                if (transform.position.x > 0 || transform.position.x < -5.5f)
-                {
-                    speedV = -speedV;
-                }
-            if (point > 0 & point < 5.5f)
-                if (transform.position.x > 5.5f || transform.position.x < 0)
-                {
-                    speedV = -speedV;
-                }
+            speedV = -speedV;
+
         }
         
-
-
+        
     }
 }
